b3b03e757eb8c7d6237817c369a94cf7d4c3d04c
beckyydo <47190773+beckyydo@users.noreply.github.com>
2021-02-13 23:55:51 -0500

    Update logic.js

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,55 +1,106 @@
-// Creating map object
-var myMap = L.map("mapid", {
-    center: [36.1699, -115.1398],
-    zoom: 4
-});
-
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Satelite Layer
+var satelite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "mapbox/satellite-v9",
     accessToken: API_KEY
-}).addTo(myMap);
+});
+// Dark Layer
+var dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/dark-v10",
+    accessToken: API_KEY
+});
+// Outdoor Layer
+var outdoor = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+});
 
-url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson?$limit=100"
+// Define Base Maps
+var baseMaps = {'Satelite': satelite, 'Dark': dark, 'Outdoors': outdoor};
 
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson?"
+
+tectonic_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
+
+var earthquakeMarkers = []
+var tectonicMarkers = []
+
+// Create tectonic line markers 
+d3.json(tectonic_url, function(response){
+
+    response.features.forEach(function(feature){
+
+        tectonicMarkers.push(L.polygon(feature.geometry.coordinates[0], {
+            color: "yellow"
+        }));
+    });
+});
+
+// Create earthquake circle markers
 d3.json(url, function(data) {
 
     data.features.forEach(function(response){
+
         var coordinates = [response.geometry.coordinates[1], response.geometry.coordinates[0]]
-        L.circle(coordinates, {
-            color: 'black',
-            fillColor: depthColor(response.geometry.coordinates[2]),
-            fillOpacity: 0.75,
-            radius: magRadius(response.properties.mag),
-            weight: 1
-        }).bindPopup(`Place: ${response.properties.place} <br>
-                    Depth: ${response.geometry.coordinates[2]} <br>
-                    Magnitude: ${response.properties.mag} <br>
-                    (Lat, Lon): (${coordinates[0]}, ${coordinates[1]})`).addTo(myMap);
-    })
 
-    // Position Legend
-    var legend = L.control({ position: "bottomright" });
-
-    legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend");
-        div.innerHTML += "<h4>Depth Legend</h4>";
-        div.innerHTML += '<i style="background: #A3F600"></i><span>-10 to 10</span><br>';
-        div.innerHTML += '<i style="background: #DCF400"></i><span>10 to 30</span><br>';
-        div.innerHTML += '<i style="background: #F7DB11"></i><span>30 to 50</span><br>';
-        div.innerHTML += '<i style="background: #FDB72A"></i><span>50 to 70</span><br>';
-        div.innerHTML += '<i style="background: #FCA35D"></i><span>70 to 90</span><br>';
-        div.innerHTML += '<i style="background: #FF5F65"></i><span>90 +</span><br>';        
-        
-        return div;
-      };
-      
-      legend.addTo(myMap);
+        earthquakeMarkers.push(L.circle(coordinates, {
+                                color: 'black',
+                                fillColor: depthColor(response.geometry.coordinates[2]),
+                                fillOpacity: 0.75,
+                                radius: magRadius(response.properties.mag),
+                                weight: 1
+                        }).bindPopup(`Place: ${response.properties.place} <br>
+                                    Depth: ${response.geometry.coordinates[2]} <br>
+                                    Magnitude: ${response.properties.mag} <br>
+                                    (Lat, Lon): (${coordinates[0]}, ${coordinates[1]})`));
+                                });
 });
+
+// Create base layers
+var earthquakes = L.layerGroup(earthquakeMarkers);
+var tectonic = L.layerGroup(tectonicMarkers);
+
+// Set Initial Map
+var myMap = L.map("mapid", {
+    center: [36.1699, -115.1398],
+    zoom: 4,
+    layers: [satelite, earthquakes, tectonic]
+});
+
+// Create overlay maps
+var overlayMaps = { 'Earthquakes': earthquakes, 'Tectonic Plates': tectonic};
+
+// Add Legend
+// Position Legend
+var legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    div.innerHTML += "<h4>Depth Legend</h4>";
+    div.innerHTML += '<i style="background: #A3F600"></i><span>-10 to 10</span><br>';
+    div.innerHTML += '<i style="background: #DCF400"></i><span>10 to 30</span><br>';
+    div.innerHTML += '<i style="background: #F7DB11"></i><span>30 to 50</span><br>';
+    div.innerHTML += '<i style="background: #FDB72A"></i><span>50 to 70</span><br>';
+    div.innerHTML += '<i style="background: #FCA35D"></i><span>70 to 90</span><br>';
+    div.innerHTML += '<i style="background: #FF5F65"></i><span>90 +</span><br>';        
+    return div;
+};
+
+legend.addTo(myMap);
+
+// Add layers control
+L.control.layers(baseMaps, overlayMaps,{collapsed:false}).addTo(myMap);
 
 // Define function to determine color according to depth
 function depthColor(depth){
@@ -68,13 +119,12 @@ function depthColor(depth){
     else if (depth > 70 && depth <= 90){
         return '#FCA35D';
     }
-    else{
+    else {
         return '#FF5F65';
     }
-
-}
+};
 
 // Define function to determin radius according to magnitude
 function magRadius(mag){
-    return mag*30000
-}
+    return mag*30000;
+};
